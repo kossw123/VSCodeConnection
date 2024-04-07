d1c02bd4dd961dee2730f29a8609625165bc9c8b
kossw123 <kossw123@naver.com>
2024-04-07 19:45:42 +0900

    testing

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
 ﻿
+
+Init init = new Init(1, "asd");
+
+
+
+public class Init
+{
+    public int id { get; set ;}
+    public string name { get; set ;}
+
+    public Init(int id, string name)
+    {
+        this.id = id;
+        this.name = name;
+    }    
+}
